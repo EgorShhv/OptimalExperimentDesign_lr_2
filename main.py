3dc35b996c4253946d63e0c6d01cc02956268ec8
EgorShhv <egshhv@gmail.com>
2022-10-06 09:58:29 +0700

    Fixed gradient search and added comments

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from numpy.linalg import inv
 
 
 def f(x1, x2):
-    f = np.matlib.matrix([[1], [x1], [x2], [x1 * x2], [x1 * x1], [x2 * x2]])
+    f = np.matrix([[1], [x1], [x2], [x1 * x2], [x1 * x1], [x2 * x2]])
     return f
 
 
 def coef_gradient_descent(n, x, p):
+    # Проекция вектора градиента (?)
     proj = np.zeros(n)
     grad = np.zeros(n)
+
+    delta = 1e-3
 
     fout = open("new_out.txt", "w")
     solution = 0
@@ -20,33 +23,35 @@ def coef_gradient_descent(n, x, p):
         solution = 1
         q = n - np.count_nonzero(p)
 
-        M = np.matlib.zeros((6, 6))
-        for i in range(0, n):
+        M = np.zeros((6, 6))
+        for i in range(n):
             M += p[i] * ((f(x[i][0], x[i][1])) * np.transpose(f(x[i][0], x[i][1])))
-        M_1 = inv(M)
+        D = inv(M)
 
-        for i in range(0, n):
-            grad[i] = np.trace(M_1 * (f(x[i][0], x[i][1])) * np.transpose(f(x[i][0], x[i][1])))
+        # 10.801353158491526
+        for i in range(n):
+            grad[i] = np.transpose(f(x[i][0], x[i][1])) @ D @ (f(x[i][0], x[i][1]))
 
         grad /= np.linalg.norm(grad)
 
         # Вычисляем среднюю сумму компонент градиента точек, в которых p_j != 0
         # Это нужно для проверки условия о достаточности (6)
         avg = 0.0
-        for i in range(0, n):
+        for i in range(n):
             if p[i] != 0.0:
                 avg += grad[i]
         avg /= n - q
 
-        for i in range(0, n):
+        for i in range(n):
             if (p[i] != 0):
-                if (abs(grad[i] - avg) > 1e-10):
+                if abs(grad[i] - avg) > delta:
                     solution = 0
 
-        for j in range(0, n):
+        # стр. 15. Над 1 пунктом.
+        for j in range(n):
             proj[j] = grad[j] - avg
             if p[j] == 0:
-                if (proj[j] > 0):
+                if proj[j] > 0:
                     solution = 0
                 else:
                     proj[j] = 0
@@ -57,31 +62,31 @@ def coef_gradient_descent(n, x, p):
             fout.write("|proj|= " + str(np.linalg.norm(proj)) + "\n")
 
         if (solution == 0):
-            for i in range(0, n):
+            for i in range(n):
                 if (proj[i] < 0 and lambdas > - p[i] / proj[i]):
                     lambdas = - p[i] / proj[i]
 
-            for i in range(0, n):
+            for i in range(n):
                 p[i] += lambdas * proj[i]   # Шаг 5 в методичке
 
         iter += 1
 
     fout.write("SOLUTION")
     fout.write("\niter= " + str(iter) + "\np: ")
-    for i in range(0, n):
+    for i in range(n):
         fout.write(str(p[i]) + " ")
     fout.write("\ndetM= " + str(np.linalg.det(M)) + " ")
     fout.write("|proj|= " + str(np.linalg.norm(proj)) + "\n")
 
-    return p, M_1
+    return p, D
 
 
-def is_optimal(M_1):
+def is_optimal(D):
     n = 201
     max = 0
-    for i in range(0, n):
-        for j in range(0, n):
-            tr = np.trace((f(-1 + 0.01 * i, -1 + 0.01 * j) * np.transpose(f(-1 + 0.01 * i, -1 + 0.01 * j))) * M_1)
+    for i in range(n):
+        for j in range(n):
+            tr = np.trace((f(-1 + 0.01 * i, -1 + 0.01 * j) * np.transpose(f(-1 + 0.01 * i, -1 + 0.01 * j))) * D)
             if (tr > max):
                 max = tr
                 x1m = -1 + 1e-2 * i
@@ -100,6 +105,6 @@ for i in range(0, m):
         x[i * m + j][0] = -1 + 0.1 * i
         x[i * m + j][1] = -1 + 0.1 * j
 
-p, M_1 = coef_gradient_descent(n, x, p)
-max, x1m, x2m = is_optimal(M_1)
+p, D = coef_gradient_descent(n, x, p)
+max, x1m, x2m = is_optimal(D)
 print(max)
